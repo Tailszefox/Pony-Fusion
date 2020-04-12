9efb618504a75395cdde5c20aa406b200280fb96
Tailszefox <tails@tailszefox.net>
2020-04-12 06:16:38 +0200

    Changed jQuery source to cloudflare

--- index.php
+++ index.php
@@ -147,7 +147,7 @@ else
         </p>
     </div>
     </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/konami.js"></script>
     <script src="js/base.js"></script>
